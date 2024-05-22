3f751f32de7d8a36f0cf3b4061d876a9af270e68
dachii21 <vashlidaatami.gmail.com>
2024-05-22 23:20:15 +0400

    first commit

---
+++ Day 0/Lesson 1.py
@@ -0,0 +1,52 @@
+from turtle import *
+
+#we want to paint a house
+
+#step 1: draw a square
+
+width(7)
+color("cyan")
+begin_fill()
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+end_fill()
+#end of square
+
+#drawing a door
+
+forward(70)
+color("brown")
+begin_fill()
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(200, 200)
+pendown()
+
+color("brown")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+
+
+
+exitonclick()
