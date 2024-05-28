769a38613559b6fe886fc03e8600af7fcac6defe
dachii21 <vashlidaatami.gmail.com>
2024-05-28 18:49:54 +0400

    second commit

---
+++ Level 3/Homework/Homework.py
@@ -0,0 +1,12 @@
+Start = "Hi,"
+Word1 = "my"
+Word2 = "name"
+Word3 = "is"
+Word4 = "Dachi"
+Word5 = "and"
+Word6 = "I"
+Word7 = "like"
+Word8 = "programming"
+End = "a lot."
+
+print(Start, Word1, Word2, Word3, Word4, Word5, Word6, Word7, Word8, End)
