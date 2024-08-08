6d36b43b95950684467584d87ac52761310f92d0
dachii21 <vashlidaatami.gmail.com>
2024-08-08 19:57:43 +0400

    commit

--- Level 23/Classwork/Python.py
+++
@@ -1,9 +0,0 @@
-Number = input("Enter a number from 1 to 100")
-
-i = 1
-while i < Number:
-  print(i + "is not a correct number!")
-  i += 1
-
-if i == Number:
-    print("The correct Number is " + Number + "!")
