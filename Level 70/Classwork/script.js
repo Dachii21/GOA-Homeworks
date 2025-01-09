39783e348f99ce717a8fc077033d21989f127d0f
dachii21 <vashlidaatami.gmail.com>
2025-01-09 22:26:42 +0400

    commit

---
+++ Level 70/Classwork/script.js
@@ -0,0 +1,17 @@
+let username = "Dachi"
+let lastname = "Chrdileli"
+let age = "14"
+let color = "Red"
+let car = "BMW"
+
+console.log(username)
+console.log(lastname)
+console.log(age)
+console.log(color)
+console.log(car)
+
+alert(username)
+alert(lastname)
+alert(age)
+alert(color)
+alert(car)
