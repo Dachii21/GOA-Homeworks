ddce623240f32b68a0d38b840af007a38ca9d25a
dachii21 <vashlidaatami.gmail.com>
2025-01-10 23:12:27 +0400

    commit

---
+++ Level 71/Classwork/script.js
@@ -0,0 +1,23 @@
+// 1
+
+// console.log(60*60)
+// console.log(3600*24)
+// console.log(86400*30)
+
+// let second = 60
+// let minute = 60
+// let hourstotal = 24
+// let month = 30
+
+// let hour = (second*minute)
+// console.log(hour)
+// let day = (hour*hourstotal)
+// console.log(day)
+// let answer = (day*month)
+// console.log(answer)
+
+// 2
+let height = 14
+let width = 8
+let rectangle = (height*width)
+console.log(rectangle)
