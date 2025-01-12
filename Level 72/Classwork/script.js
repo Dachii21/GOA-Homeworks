db92fafdce72147e4d025020fbc7011810ef3e05
dachii21 <vashlidaatami.gmail.com>
2025-01-12 23:12:04 +0400

    commit

---
+++ Level 72/Classwork/script.js
@@ -0,0 +1,21 @@
+// let age = (prompt("შეიყვანე შენი ასაკი: "));
+
+// if(age >= 5) {
+//     console.log("შენ ხარ ბავშვი")
+// } else if(age >= 12) {
+//     console.log("შენ ხარ თინეიჯერი/მოზარდი")
+// } else if (age >= 18) {
+//     console.log("შენ ხარ სრულწლოვანი")
+// } else if (age >= 80) {
+//     console.log("დაბერდი ძმაო")
+// } else {
+//     console.log("თავიდან შეიყვანე შენი ასაკი")
+// }
+
+let number = Number(prompt("შეიყვანე რიცხვი: "));
+
+if(number % 2 == 0) {
+    console.log("ეს რიცხვი ლუწია")
+} else {
+    console.log("ეს რიცხვი კენტია")
+}
