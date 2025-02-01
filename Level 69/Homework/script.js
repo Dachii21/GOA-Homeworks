5e4229e5843be18ef2746b284d893b436da1f4af
dachii21 <vashlidaatami.gmail.com>
2025-02-01 22:35:05 +0400

    commit

---
+++ Level 69/Homework/script.js
@@ -0,0 +1,41 @@
+alert(1+1);
+alert(1-1);
+alert(2*3);
+alert(6/2);
+alert(2**5);
+
+console.log(1+1);
+console.log(1-1);
+console.log(2*3);
+console.log(6/2);
+console.log(2**5);
+
+alert("Hello" + " " + "World");
+
+console.log("Hello" + " " + "World");
+
+alert("The world will be destroyed in 10 seconds")
+alert("The world will be destroyed in 9 seconds")
+alert("The world will be destroyed in 8 seconds")
+alert("The world will be destroyed in 7 seconds")
+alert("The world will be destroyed in 6 seconds")
+alert("The world will be destroyed in 5 seconds")
+alert("The world will be destroyed in 4 seconds")
+alert("The world will be destroyed in 3 seconds")
+alert("The world will be destroyed in 2 seconds")
+alert("The world will be destroyed in 1 second")
+
+console.log("The World will be destroyed in 10 seconds");
+console.log("The world will be destroyed in 9 seconds");
+console.log("The world will be destroyed in 8 seconds");
+console.log("The world will be destroyed in 7 seconds");
+console.log("The world will be destroyed in 6 seconds");
+console.log("The world will be destroyed in 5 seconds");
+console.log("The world will be destroyed in 4 seconds");
+console.log("The world will be destroyed in 3 seconds");
+console.log("The world will be destroyed in 2 seconds");
+console.log("The world will be destroyed in 1 second");
+
+// Alert არის ფანჯარა რომელიც გამოსახავს ინფორმაციას, მაგალითად რიცხვები და ტექსტი.
+// Console.log არის ინფორმაცია რომელიც Console-ში გამოისახება, მასაც შეუძლია რიცხვების და ტექსტის გამოსახულება.
+// ასევე ორივე შეგვიძლია კალკულატორადაც გამოვიყენოთ სწორი script-ით.
