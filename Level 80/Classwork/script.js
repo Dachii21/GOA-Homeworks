128d295b007614a4e8137a16caaee47039388591
dachii21 <vashlidaatami.gmail.com>
2025-02-09 22:35:52 +0400

    commit

---
+++ Level 80/Classwork/script.js
@@ -0,0 +1,37 @@
+// let i = 50;
+
+// while(i > 0) {
+
+//     console.log("GOA");
+
+//     i--;
+// }
+
+// let i = 20;
+
+// while (i >= 0) {
+//     console.log(i);
+//     i--;
+// }
+
+// let i = 1;
+
+// while (i <= 20) {
+
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+
+//     i++;
+// }
+
+let i = 1;
+
+while (i <= 100) {
+
+    if (i % 5 === 0) {
+        console.log(i);
+    }
+
+    i++;
+}
