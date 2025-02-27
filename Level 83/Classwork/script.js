ff297b377ad649635fee309a79fb2f6c74adda9a
dachii21 <vashlidaatami.gmail.com>
2025-02-27 21:33:43 +0400

    commit

---
+++ Level 83/Classwork/script.js
@@ -0,0 +1,14 @@
+// function greet() {
+//     let name = "Dachi"
+//     console.log("Welcome," + " " + name);
+// }
+
+// greet()
+
+function convert(feet) {
+    let feet = 8
+    let inches = feet * 12
+    return inches;
+}
+
+convert()
---
+++ Level 84/Classwork/Script.js
@@ -0,0 +1,24 @@
+// function CheckUserInfo(name, lastname, age, birthYear) {
+//     return [name, lastname, age, birthYear];
+// }
+
+// let name = "Dachi";
+// let lastname = "Chrdileli";
+// let age = 14;
+// let birthYear = 2010;
+
+// let result = CheckUserInfo(name, lastname, age, birthYear);
+
+// if (result[0] === name && result[1] === lastname && result[2] === age && result[3] === birthYear) {
+//     console.log("The info is correct");
+// } else {
+//     console.log("The info is incorrect");
+// }
+
+// function area(length, width) {
+//     return length * width;
+// }
+//  let length = 7;
+//  let width = 4;
+//  let result = area(length, width);
+//  console.log("The answer is " + result);
