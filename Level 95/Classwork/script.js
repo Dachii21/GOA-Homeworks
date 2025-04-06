629cf461cf0d025d2fab773e47c465772c7e8336
dachii21 <vashlidaatami.gmail.com>
2025-04-06 22:50:47 +0400

    commit

---
+++ Level 95/Classwork/script.js
@@ -0,0 +1,12 @@
+let date = new Date();
+
+console.log("Date and Time:" + " ", date);
+
+date.setFullYear(2019);
+date.setMonth(12);
+date.setDate(31);
+date.setHours(10);
+date.setMinutes(59);
+date.setSeconds(10);
+
+console.log("Modified Date and Time:" + " ", date);
