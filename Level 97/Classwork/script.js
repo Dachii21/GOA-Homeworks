c6c3a9cc503b8c0b5feb22e1767a8480350a2899
dachii21 <vashlidaatami.gmail.com>
2025-04-19 23:19:29 +0400

    commit

---
+++ Level 97/Classwork/script.js
@@ -0,0 +1,15 @@
+const mainheader = document.getElementById("main-heading");
+const firstParagraph = document.querySelector(".first-paragraph");
+const secondParagraph = document.querySelectorAll("p")[1];
+
+mainHeading.style.color = "orange";
+mainHeading.style.fontSize = "24px";
+mainHeading.textContent = "Updated Heading";
+
+firstParagraph.style.color = "green";
+firstParagraph.style.fontSize = "16px";
+firstParagraph.textContent = "Updated First Paragraph";
+
+secondParagraph.style.color = "blue";
+secondParagraph.style.fontSize = "14px";
+secondParagraph.textContent = "Updated Second Paragraph";
